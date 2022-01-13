20ce18f7108a05671feea8699315b832dcc36311
unknown <piero.pulcrano@mcengineering.eu>
2022-01-13 19:02:29 +0100

    add reccomandatation vod search test

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -5,17 +5,10 @@ cy.getRandomNumber = (rangeMin, rangeMax) => {
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
 
-
-cy.getTomorrowDate = () => {
-    let $today = new Date();
-    let $tomorrow = new Date($today);
-    $tomorrow.setDate($today.getDate() + 1);
-
-    let dd = $tomorrow.getDate();
-    let mm = $tomorrow.getMonth()+1;
-    let yyyy = $tomorrow.getFullYear();
-    let tomorrowDate = (dd < 10 ? '0' + dd : dd) + '/' + (mm < 10 ? '0' + mm : mm) + '/' + yyyy + ' 5:20 PM';
-    return tomorrowDate; // prints 28/04/2020
-
+cy.generateFutureDate= (plusDate,format) =>{
+    let tomorrow = Cypress.dayjs().add(plusDate, 'day');
+    return Cypress.dayjs(tomorrow).format(format);
+    
 }
+
 
