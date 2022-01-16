b0e9acfb9fb2446127773ef2aaff591e40b9f366
FINCONSGROUP\fabio.mangano <fabio.mangano@finconsgroup.com>
2022-01-16 18:01:58 +0100

    End check update lin rec scheduled for the future

--- cypress/integration/update-recommendation-lin.js
+++ cypress/integration/update-recommendation-lin.js
@@ -2,12 +2,20 @@ import linRec from '../fixtures/lin-recommendation';
 
 describe('Testing lin recommendation', () => {
   it('Check update lin recommendation', () => {
-    // create lin recommendation to update
     let toUpdate;
     let eventId;
 
-    // intercept call to create new lin recommendation to update
     cy.intercept({ method: 'POST', path: '/recommendations' }).as('createLin');
+
+    cy.intercept({
+      method: 'GET',
+      path: 'http://localhost:3001/recommendations*',
+    }).as('loadLin');
+
+    // visit the schedule page
+    cy.visit('http://localhost:3000/schedule');
+
+    // intercept call to create new lin recommendation to update
 
     cy.request({
       method: 'POST',
@@ -18,90 +26,75 @@ describe('Testing lin recommendation', () => {
       toUpdate = response.body.id;
       eventId = `eventId-${toUpdate}`;
 
-      // visit the schedule page
-      cy.visit('http://localhost:3000/schedule');
-
       // get lin rec to update;
       cy.get(`[data-testid="${eventId}"]`).click();
 
+      // intercept does not work goddam
+      cy.wait(2000);
+
       // pick a random cluster
       let randomIndexClusterVal = cy.generateRandomCluster();
-      // create tomorrow date as startDateTime
-      let startDateTime = cy.generateFutureDate(2, 'DD/MM/YYYY h:mm A');
-      // create a date five days after tomorrow date as endDateTime
-      let endDateTime = cy.generateFutureDate(5, 'DD/MM/YYYY h:mm A');
+
+      cy.selectRandomCluster(randomIndexClusterVal);
+      // select sd slot row
+      cy.get('[data-testid="sd-slot-row"]')
+        .find('[data-testid="AddCircleIcon"] > path')
+        .each(($el, index, list) => {
+          // for each slot
+          // click on add button
+          cy.wrap($el).click({ force: true });
+          // get the opened modal
+          cy.get('[data-test="search-lin-modal"]').within(() => {
+            // search lin event title
+            cy.get('input[type="text"]').first().type("L'immortale");
+            // insert startDate
+            cy.get('input[type="text"]').last().type('19/01/2022 6:29 PM');
+            // click on search button
+            cy.contains('Search').click();
+            // it should be at least one result
+            cy.get('.MuiDataGrid-row').its('length').should('be.gt', 0);
+            // click on first row
+            cy.get('.MuiDataGrid-row').first().click();
+            // click on submit
+            cy.contains('Select').click();
+          });
+        });
+
+      // select hd slot row
+      cy.get('[data-testid="hd-slot-row"]')
+        .find('[data-testid="AddCircleIcon"] > path')
+        .each(($el, index, list) => {
+          // for each slot
+          // click on add button
+          cy.wrap($el).click({ force: true });
+          // get the opened modal
+          cy.get('[data-test="search-lin-modal"]').within(() => {
+            // search lin title
+            cy.get('input[type="text"]').first().type('Avatar');
+            // insert startDate
+            cy.get('input[type="text"]').last().type('19/01/2022 6:29 PM');
+            // click on search button
+            cy.contains('Search').click();
+            // it should be at least one result
+            cy.get('.MuiDataGrid-row').its('length').should('be.gt', 0);
+            // click on first row
+            cy.get('.MuiDataGrid-row').first().click();
+            // click on submit button
+            cy.contains('Select').click();
+          });
+        });
+
+      // invia
+      cy.contains('Update').click();
+
+      cy.contains('Lin Updated');
+      // delete created lin
+
+      cy.wait(2000);
 
       cy.request('DELETE', `http://localhost:3001/recommendations/1000`);
+
+      cy.visit('http://localhost:3000/schedule');
     });
-
-    /* // click on create new lin button
-    cy.contains('NEW LIN').click();
-    // select user cluster
-    cy.selectRandomCluster(randomIndexClusterVal);
-    // fill recommendation startDateTime
-    cy.get('input[type="text"]').first().type(startDateTime);
-    // fill recommendation endDateTime
-    cy.get('input[type="text"]').last().type(endDateTime);
-    // select sd slot row
-    cy.get('[data-testid="sd-slot-row"]')
-      .find('[data-testid="AddCircleIcon"] > path')
-      .each(($el, index, list) => {
-        // for each slot
-        // click on add button
-        cy.wrap($el).click({ force: true });
-        // get the opened modal
-        cy.get('[data-test="search-lin-modal"]').within(() => {
-          // search lin event title
-          cy.get('input[type="text"]').first().type("L'immortale");
-          // insert startDate
-          cy.get('input[type="text"]').last().type('19/01/2022 6:29 PM');
-          // click on search button
-          cy.contains('Search').click();
-          // it should be at least one result
-          cy.get('.MuiDataGrid-row').its('length').should('be.gt', 0);
-          // click on first row
-          cy.get('.MuiDataGrid-row').first().click();
-          // click on submit
-          cy.contains('Select').click();
-        });
-      });
-
-    // select hd slot row
-    cy.get('[data-testid="hd-slot-row"]')
-      .find('[data-testid="AddCircleIcon"] > path')
-      .each(($el, index, list) => {
-        // for each slot
-        // click on add button
-        cy.wrap($el).click({ force: true });
-        // get the opened modal
-        cy.get('[data-test="search-lin-modal"]').within(() => {
-          // search lin title
-          cy.get('input[type="text"]').first().type('Avatar');
-          // insert startDate
-          cy.get('input[type="text"]').last().type('19/01/2022 6:29 PM');
-          // click on search button
-          cy.contains('Search').click();
-          // it should be at least one result
-          cy.get('.MuiDataGrid-row').its('length').should('be.gt', 0);
-          // click on first row
-          cy.get('.MuiDataGrid-row').first().click();
-          // click on submit button
-          cy.contains('Select').click();
-        });
-      });
-
-    // invia
-    cy.contains('Create').click();
-
-    cy.wait('@createLin').should(({ req, response }) => {
-      expect(response.statusCode).to.equal(201);
-      // check that notification appears
-      cy.contains('Lin Created');
-      // delete created lin
-      cy.request(
-        'DELETE',
-        `http://localhost:3001/recommendations/${response.body.id}`,
-      );
-    }); */
   });
 });
