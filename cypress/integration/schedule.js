43e3ba2890ffd52be19a5c087ce949ccaa56adbf
unknown <piero.pulcrano@mcengineering.eu>
2022-01-17 16:21:53 +0100

    remove cache for the intercept on schedule.js

--- cypress/integration/schedule.js
+++ cypress/integration/schedule.js
@@ -1,48 +1,27 @@
 /// <reference types="cypress" />
 
-
-
 describe('Testing Schedule Page', () => {
-
-    beforeEach(() => {
-        // Cypress starts out with a blank slate for each test
-        // so we must tell it to visit our website with the `cy.visit()` command.
-        // Since we want to visit the same URL at the start of all our tests,
-        // we include it in our beforeEach function so that it runs before each test
-        cy.visit('http://localhost:3000/schedule');
-
-
-
-    })
-
-    it('Correct redirect', () => {
-        //check redirect su /fallback
-        cy.url().should('include', '/schedule');
-
-    })
-
-    
-    it('Check render all reccomandation', () => {
-
-        cy.intercept({ method: 'GET', url: '**/recommendations*' }).as('searchRequest');
-
-        cy.wait('@searchRequest').then((interception) => {
-            //se non è tornato 304
-            if (interception.response.statusCode = 200) {
-                let reccArr = interception.response.body;
-                for (var i = 0; i < reccArr.length; i++) {
-                    //controlle che abbia renderizzato tutti gli elementi
-                    cy.get('[data-testId="eventId-' + reccArr[i].id + '"]').should('have.length', 1);
-                    //open the modal
-                    cy.get('[data-testId="eventId-' + reccArr[i].id + '"]').contains(reccArr[i].type).trigger('mouseover',{force: true})
-                                       
-                }
-
-            }
-
-        });
- 
-
-    })
-
-})
+  it('Check render all reccomandation', () => {
+    cy.intercept({ method: 'GET', url: '**/recommendations*' }, (req) => {
+      delete req.headers['if-none-match'];
+    }).as('searchRequest');
+    cy.visit('http://localhost:3000/schedule');
+    cy.wait('@searchRequest').then((interception) => {
+      //se non è tornato 304
+      if (interception.response.statusCode == 200) {
+        let reccArr = interception.response.body;
+        for (var i = 0; i < reccArr.length; i++) {
+          //controlle che abbia renderizzato tutti gli elementi
+          cy.get('[data-testId="eventId-' + reccArr[i].id + '"]').should(
+            'have.length',
+            1,
+          );
+          //open the modal
+          cy.get('[data-testId="eventId-' + reccArr[i].id + '"]')
+            .contains(reccArr[i].type)
+            .trigger('mouseover', { force: true });
+        }
+      }
+    });
+  });
+});
