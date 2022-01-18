e1c790dc04b042e8404149337c4db58326277e8f
FINCONSGROUP\fabio.mangano <fabio.mangano@finconsgroup.com>
2022-01-18 14:23:50 +0100

    Extended e2e test for lin recommendation

--- cypress/fixtures/lin-recommendation.js
+++ cypress/fixtures/lin-recommendation.js
@@ -1,4 +1,4 @@
-const linRec = {
+export const futurelinRec = {
   id: '1000',
   cluster: 'C1',
   type: 'LIN',
@@ -60,4 +60,64 @@ const linRec = {
   ],
 };
 
-export default linRec;
+export const presentlinRec = {
+  id: '1002',
+  cluster: 'C1',
+  type: 'LIN',
+  validFrom: cy.generateFutureDate(0),
+  validTo: cy.generateFutureDate(2),
+  recommendation: [
+    {
+      id: '61GA',
+      type: 'LIN',
+      title: 'Avatar',
+      position: '1',
+      startProgram: '2021-12-26T15:00:00Z',
+      channel: 'Sky Cinema HD',
+      linearSummaryLong:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      thumbnailType: '',
+      resolution: 'HD',
+      verticalImageUrl: '',
+    },
+    {
+      id: '61GK',
+      type: 'LIN',
+      title: "L'immortale",
+      position: '1',
+      startProgram: '2021-12-26T15:00:00Z',
+      channel: 'Sky Cinema HD',
+      linearSummaryLong:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      thumbnailType: '',
+      resolution: 'SD',
+      verticalImageUrl: '',
+    },
+    {
+      id: '61GP',
+      type: 'LIN',
+      title: 'Avengers',
+      position: '2',
+      startProgram: '2021-12-26T15:00:00Z',
+      channel: 'Sky Cinema HD',
+      linearSummaryLong:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      thumbnailType: '',
+      resolution: 'HD',
+      verticalImageUrl: '',
+    },
+    {
+      id: '61GL',
+      type: 'LIN',
+      title: 'Benvenuti al sud',
+      position: '2',
+      startProgram: '2021-12-26T15:00:00Z',
+      channel: 'Sky Cinema HD',
+      linearSummaryLong:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      thumbnailType: '',
+      resolution: 'SD',
+      verticalImageUrl: '',
+    },
+  ],
+};
--- cypress/integration/update-recommendation-lin.js
+++ cypress/integration/update-recommendation-lin.js
@@ -1,7 +1,7 @@
-import linRec from '../fixtures/lin-recommendation';
+import { presentlinRec, futurelinRec } from '../fixtures/lin-recommendation';
 
 describe('Testing lin recommendation', () => {
-  it('Check update lin recommendation', () => {
+  it('Check update lin recommendation scheduled for the future', () => {
     let toUpdate;
     let eventId;
 
@@ -12,7 +12,7 @@ describe('Testing lin recommendation', () => {
     cy.request({
       method: 'POST',
       url: 'http://localhost:3001/recommendations',
-      body: linRec,
+      body: futurelinRec,
     }).then((response) => {
       expect(response.status).to.eql(201);
       toUpdate = response.body.id;
@@ -81,6 +81,49 @@ describe('Testing lin recommendation', () => {
 
       // delete created notification
       cy.request('DELETE', `http://localhost:3001/recommendations/1000`);
+
+      // force refresh of the page
+      cy.visit('http://localhost:3000/schedule');
+    });
+  });
+
+  it('Check update lin recommendation scheduled for the present', () => {
+    let toUpdate;
+    let eventId;
+    // create a date five days after tomorrow date as endDateTime
+    const endDateTime = cy.generateFutureDate(5, 'DD/MM/YYYY h:mm A');
+
+    // visit the schedule page
+    cy.visit('http://localhost:3000/schedule');
+
+    // create lin rec to update
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:3001/recommendations',
+      body: presentlinRec,
+    }).then((response) => {
+      expect(response.status).to.eql(201);
+      toUpdate = response.body.id;
+      eventId = `eventId-${toUpdate}`;
+
+      cy.visit('http://localhost:3000/schedule');
+
+      // get lin rec to update;
+      cy.get(`[data-testid="${eventId}"]`).click();
+
+      // pick a random cluster
+      // fill recommendation endDateTime
+      cy.get('input[type="text"]').clear();
+      cy.get('input[type="text"]').last().type(endDateTime);
+
+      // submit update
+      cy.contains('Update').click();
+
+      // check for notification
+      cy.contains('Lin Updated');
+
+      // delete created notification
+      cy.request('DELETE', `http://localhost:3001/recommendations/${toUpdate}`);
 
       // force refresh of the page
       cy.visit('http://localhost:3000/schedule');
