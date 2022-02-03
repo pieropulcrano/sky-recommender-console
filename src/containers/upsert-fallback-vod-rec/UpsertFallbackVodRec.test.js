2cbb1b2b3ffa4397cd9c14792af30f5d5b178fb8
unknown <piero.pulcrano@mcengineering.eu>
2022-02-03 11:45:59 +0100

    add UpsertFallbackVodRec.test

---
+++ src/containers/upsert-fallback-vod-rec/UpsertFallbackVodRec.test.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import * as vodRecProvider from '../../providers/vod-rec-provider/VodRecProvider';
+import UpsertFallbackVodRec from './UpsertFallbackVodRec';
+import { prepareFallbackVodRec } from './UpsertFallbackVodRec.helpers';
+import fallbackReccMock from '../../../fixtures/fallback-recc-mock.json';
+
+const MockUpsertFallbackVodRec = ({ ...props }) => {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <UpsertFallbackVodRec {...props} />
+    </LocalizationProvider>
+  );
+};
+
+let mockedAddAlert = jest.fn();
+jest.mock('../../hooks/useNotification', () => () => ({
+  addAlert: mockedAddAlert,
+}));
+
+describe('Upsert Fallback Vod Rec', () => {
+  let props;
+  const updateBtn = 'Update';
+  const alertErrorSaving = {
+    text: 'An error occurred while saving the Vod Fallback recommendation.',
+    title: 'Vod Fallback saving error',
+    type: 'error',
+    id: expect.anything(),
+  };
+  const alertErrorLoading = {
+    text: 'An error occured during the loading of the fallback vod rec.',
+    title: 'Vod Fallback loading failed',
+    type: 'warning',
+    id: expect.anything(),
+  };
+  const alertOkSaving = {
+    text: 'Vod Fallback was successfully updated.',
+    title: ` Vod Fallback updated`,
+    type: 'success',
+    data_test: 'vod-fallback-ok-not',
+    id: expect.anything(),
+  };
+
+  beforeEach(() => {
+    props = {
+      fallbackVodRec: undefined,
+      handleAlertFallback: jest.fn(),
+    };
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should display loading spinner', async () => {
+    render(<MockUpsertFallbackVodRec {...props} />);
+
+    expect(screen.getByRole('progressbar')).toBeInTheDocument();
+  });
+
+  describe('It should display error notification', () => {
+    it('if an error returnig after update', async () => {
+      props.fallbackVodRec = fallbackReccMock;
+      const mockedUpdateFallbackVodRec = jest.fn(() => {
+        throw new Error('error');
+      });
+
+      jest
+        .spyOn(vodRecProvider, 'updateFallbackVodRec')
+        .mockImplementation(mockedUpdateFallbackVodRec);
+
+      render(<MockUpsertFallbackVodRec {...props} />);
+
+      const updateButton = screen.queryByText(updateBtn);
+
+      await waitFor(() => {
+        fireEvent.click(updateButton);
+        expect(mockedUpdateFallbackVodRec).toHaveBeenCalledTimes(1);
+      });
+
+      await waitFor(() => {
+        expect(mockedUpdateFallbackVodRec).toHaveBeenCalledWith(
+          prepareFallbackVodRec(fallbackReccMock[0].id, fallbackReccMock[0]),
+        );
+      });
+
+      await waitFor(() => {
+        expect(mockedAddAlert).toHaveBeenCalledWith(alertErrorSaving);
+      });
+    });
+
+    it('if an error returnig during load', async () => {
+      props.fallbackVodRecError = {};
+
+      render(<MockUpsertFallbackVodRec {...props} />);
+
+      await waitFor(() => {
+        expect(mockedAddAlert).toHaveBeenCalledWith(alertErrorLoading);
+      });
+    });
+  });
+
+  describe('It should display ok notification', () => {
+    it('if update correctly', async () => {
+      props.fallbackVodRec = fallbackReccMock;
+      const mockedUpdateFallbackVodRec = jest.fn(() => {
+        return {
+          updatedFallbackRecommendation: fallbackReccMock[0],
+        };
+      });
+
+      jest
+        .spyOn(vodRecProvider, 'updateFallbackVodRec')
+        .mockImplementation(mockedUpdateFallbackVodRec);
+
+      render(<MockUpsertFallbackVodRec {...props} />);
+
+      const updateButton = screen.queryByText(updateBtn);
+
+      await waitFor(() => {
+        fireEvent.click(updateButton);
+        expect(mockedUpdateFallbackVodRec).toHaveBeenCalledTimes(1);
+      });
+
+      await waitFor(() => {
+        expect(mockedUpdateFallbackVodRec).toHaveBeenCalledWith(
+          prepareFallbackVodRec(fallbackReccMock[0].id, fallbackReccMock[0]),
+        );
+      });
+
+      await waitFor(() => {
+        expect(mockedAddAlert).toHaveBeenCalledWith(alertOkSaving);
+      });
+    });
+  });
+});
