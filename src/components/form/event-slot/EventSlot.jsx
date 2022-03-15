46126038d7a5933f9ec2245009b0e22a60675ea2
unknown <piero.pulcrano@mcengineering.eu>
2022-03-15 13:58:24 +0100

    Channel name only on form linear

--- src/components/form/event-slot/EventSlot.jsx
+++ src/components/form/event-slot/EventSlot.jsx
@@ -105,7 +105,7 @@ const EventSlot = ({ name, handleOpen, type, disabled, data_test_slot }) => {
           </Tooltip>
         </Grid>
       )}
-      {value.channel && (
+      {value.channel && value.type === 'LIN' && (
         <Grid container>
           <EventChannel noWrap>{value.channel}</EventChannel>
         </Grid>
